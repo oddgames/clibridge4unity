9a847d4d27bb61404fad1f0aa0d9139c935d9a3c
ODD Games <david@oddgames.com.au>
2026-05-01 06:13:32 +0800

    Release v1.0.94

--- Package/Editor/Core/BridgeServer.cs
+++ Package/Editor/Core/BridgeServer.cs
@@ -21,7 +21,7 @@ namespace clibridge4unity
     [InitializeOnLoad]
     public static class BridgeServer
     {
-        public const string Version = "1.0.93";
+        public const string Version = "1.0.94";
 
         private static CancellationTokenSource serverCts;
         private static readonly object serverLock = new object();
